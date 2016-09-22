be3c15722e2667ffabcf9efcb3e1b6d16d3c535d
Eden Ding <Eden0097@gmail.com>
2016-09-21 22:51:25 -0700

    First commit; added new posts, users, and comment sections to our database

---
+++ db/migrate/20160922054322_add_comments.rb
@@ -0,0 +1,10 @@
+class AddComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.integer :user_id, :post_id
+
+      t.timestamps
+    end
+  end
+end
